239f3d521408385d3aa2d21d0e97346c57be0663
Vlad Levchenko <kosras.96@gmail.com>
2021-07-21 21:12:48 +0300

    added files back regarding new git root folder location

---
+++ Pokemon/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Pokemon.Services.Exceptions;
+using Pokemon.Services.Models;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Pokemon.Middlewares
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
+        {
+            _logger = logger;
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Something went wrong: {ex}");
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+
+        private static async Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+
+            string message;
+            if (exception is PokemonException)
+            {
+                var pokemonException = exception as PokemonException;
+                context.Response.StatusCode = (int)pokemonException.StatusCode;
+                message = pokemonException.Message;
+            }
+            else
+            {
+                // not expected exception so we return 500
+                message = "Internal Server Error.";
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            }
+
+            await context.Response.WriteAsync(new ErrorDetails()
+            {
+                StatusCode = context.Response.StatusCode,
+                Message = message
+            }.ToString());
+        }
+    }
+}
---
+++ Pokemon/Startup.cs
@@ -0,0 +1,68 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Pokemon.Middlewares;
+using Pokemon.Services;
+using System;
+
+namespace Pokemon
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSwaggerGen();
+            services.AddHttpClient<IPokemonProvider, PokemonProvider>(client =>
+            {
+                client.BaseAddress = new Uri(Configuration["PokemonUrl"]);
+            });
+            services.AddHttpClient<IShakespeareTranslationProvider, ShakespeareTranslationProvider>(client =>
+            {
+                client.BaseAddress = new Uri(Configuration["ShakespeareUrl"]);
+            });
+            services.AddHttpClient<IYodaTranslationProvider, YodaTranslationProvider>(client =>
+            {
+                client.BaseAddress = new Uri(Configuration["YodaUrl"]);
+            });
+            services.AddScoped<ITranslationPokemonService, TranslationPokemonService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseMiddleware<ExceptionMiddleware>();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Pokemon API V1");
+                c.RoutePrefix = string.Empty;
+            });
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}

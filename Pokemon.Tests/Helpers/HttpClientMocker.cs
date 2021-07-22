485b1866d82c37b0e73b329dd52ec8d713875078
Vlad Levchenko <kosras.96@gmail.com>
2021-07-22 19:21:26 +0300

    implemented unit tests for PokemonProvider class

---
+++ Pokemon.Tests/Helpers/HttpClientMocker.cs
@@ -0,0 +1,38 @@
+﻿using Moq;
+using Moq.Protected;
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Pokemon.Tests.Helpers
+{
+    public class HttpClientMocker
+    {
+        public static HttpClient MockHttpClient(HttpResponseMessage message)
+        {
+            //var mockClient = new Mock<HttpClient>();
+            //mockClient.Setup(client => client.SendAsync(It.IsAny<HttpRequestMessage>(), It.IsAny<CancellationToken>()))
+            //    .ReturnsAsync(message);
+
+            //var client = mockClient.Object;
+            //client.BaseAddress = new Uri("https://test/");
+            //return client;
+            var mockHttpMessageHandler = new Mock<HttpMessageHandler>();
+            mockHttpMessageHandler
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(() => message);
+
+            var httpClient = new HttpClient(mockHttpMessageHandler.Object)
+            {
+                BaseAddress = new Uri("https://test/")
+            };
+
+            return httpClient;
+        }
+    }
+}
---
+++ Pokemon.Tests/PokemonProviderTests.cs
@@ -0,0 +1,80 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Newtonsoft.Json;
+using Pokemon.Services;
+using Pokemon.Services.Exceptions;
+using Pokemon.Services.Models;
+using Pokemon.Tests.Helpers;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Pokemon.Tests
+{
+    public class PokemonProviderTests
+    {
+        [Fact]
+        public async Task PokemonProvider_GetPokemonAsync_ReturnsDeserializedPokemon()
+        {
+            // arrange
+            var message = new HttpResponseMessage
+            {
+                StatusCode = HttpStatusCode.OK,
+            };
+            var responseModel = new RawPokemonResponse
+            {
+                name = "asd",
+                flavor_text_entries = new List<FlavorTextEntry> { new FlavorTextEntry { language = new Language { name = "en" }, flavor_text = "sometext" } },
+                habitat = new Habitat { name = "somename" },
+                is_legendary = false
+            };
+            message.Content = new StringContent(JsonConvert.SerializeObject(responseModel));
+
+            var httpclient = HttpClientMocker.MockHttpClient(message);
+            var logger = Mock.Of<ILogger<PokemonProvider>>();
+            var provider = new PokemonProvider(httpclient, logger);
+
+            CancellationTokenSource cancelTokenSource = new();
+            CancellationToken token = cancelTokenSource.Token;
+
+            // act
+            var result = await provider.GetPokemonAsync("ditto", token);
+
+            // assert
+            result.Should().NotBeNull();
+            result.Name.Should().Be(responseModel.name);
+            result.Description.Should().Be(responseModel.flavor_text_entries[0].flavor_text);
+            result.Habitat.Should().Be(responseModel.habitat.name);
+            result.IsLegendary.Should().Be(responseModel.is_legendary);
+        }
+
+        [Theory]
+        [InlineData(HttpStatusCode.NotFound, HttpStatusCode.NotFound)]
+        [InlineData(HttpStatusCode.TooManyRequests, HttpStatusCode.ServiceUnavailable)]
+        public async void PokemonProvider_GetPokemonAsync_ThrowsException(HttpStatusCode requestCode, HttpStatusCode exceptionCode)
+        {
+            // arrange
+            var message = new HttpResponseMessage
+            {
+                StatusCode = requestCode,
+            };
+
+            var httpclient = HttpClientMocker.MockHttpClient(message);
+            var logger = Mock.Of<ILogger<PokemonProvider>>();
+            var provider = new PokemonProvider(httpclient, logger);
+
+            CancellationTokenSource cancelTokenSource = new();
+            CancellationToken token = cancelTokenSource.Token;
+
+            // act
+            provider.Invoking(y => y.GetPokemonAsync("ditto", token))
+            // assert
+            .Should().Throw<PokemonException>()
+            .Where(e => e.StatusCode == exceptionCode);
+        }
+    }
+}
